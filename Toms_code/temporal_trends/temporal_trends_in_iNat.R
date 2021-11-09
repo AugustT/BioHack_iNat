644a5432ba0e04d56a334fbbb3ee688529a6339d
Tom August <tomaug@ceh.ac.uk>
2021-11-09 14:27:21 +0000

    update temporal code

--- Toms_code/temporal_trends/temporal_trends_in_iNat.R
+++ Toms_code/temporal_trends/temporal_trends_in_iNat.R
@@ -10,14 +10,14 @@ data_raw <- read.csv('../../2_Generating_iNat_RM_Baseline/data/observations/draf
 # Format the data
 data_raw$date <- as.Date(data_raw$date)
 
-str(data_raw)
-
-
-
 # Explore the temporal variation ----
 ggplot(data_raw,
        aes(x = date)) +
-  geom_histogram()
+  geom_histogram() +
+  scale_x_date(date_breaks = "1 month", 
+               labels = date_format("%b-%Y"),
+               limits = as.Date(c('2017-01-01','2022-01-01'))) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 # Define time periods ----
 
